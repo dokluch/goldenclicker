8259b214f0c66d83d0a95758cb60f88579ac46f4
Nik Ska <dokluch@gmail.com>
2023-04-26 11:29:48 +0400

    Updated model to support bunnies and other objects

--- app.py
+++ app.py
@@ -5,44 +5,92 @@ import time
 import pyautogui
 import numpy as np
 from classes import Counter
-
+import asyncio
 from yolo_ops import build_model, detect, format_yolov5, load_classes, wrap_detection
+from datetime import datetime
+from pathlib import Path
 
 class_list = load_classes()
 colors = [(255, 255, 0), (0, 255, 0), (0, 255, 255), (255, 0, 0)]
 is_cuda = False
 net = build_model(is_cuda)
+default_pause = 5
+
+def format_datetime_for_filename():
+    now = datetime.now()
+    return now.strftime("%Y-%m-%d_%H-%M-%S")
 
 
-
-def click_coorditates(x, y):
-    pyautogui.moveTo(x, y)
+async def click_coorditates(coordinates):
+    pyautogui.moveTo(coordinates[0], coordinates[1])
     # Click the left mouse button
     pyautogui.click(button='left')
-    time.sleep(0.01)
+    time.sleep(0.005)
 
 # Define endpoint to accept file uploads
-def predict(png_data, goldenclicks):
+async def predict(png_data, goldenclicks, wrathclicks, handclicks, deerclicks, spookyclicks, bunnyclicks):
     """
     Get YOLO predictions and click if there are boxes with suitable type
     """
+
     frame = cv2.imdecode(np.frombuffer(png_data, dtype=np.uint8), cv2.IMREAD_COLOR)
     inputImage = format_yolov5(frame)
     outs = detect(inputImage, net)
 
     class_ids, confidences, boxes = wrap_detection(inputImage, outs[0])
 
+
     if boxes:
         for class_id, box in zip(class_ids, boxes):
-            if class_id != 1:
-                continue
+            coordinates = (box[0] + box[2] / 2, box[1] + box[3] / 2)
 
-            goldenclicks.increment()
-            click_coorditates(box[0], box[1])
-            print(f"Golden cookies clicked: {goldenclicks.counter}")
+            if class_id == 1:
+                #Golden Cookie
+                goldenclicks.increment()
+                await click_coorditates(coordinates)
+                print(goldenclicks)
+            
+            if class_id == 2:
+                #Wrath Cookie
+                wrathclicks.increment()
+                await click_coorditates(coordinates)
+                print(wrathclicks)
+
+            if class_id == 3:
+                # Hand of Fate Spell
+                handclicks.increment()
+                await click_coorditates(coordinates)
+                print(handclicks)
+
+            if class_id == 5:
+                # Deers clicked
+                deerclicks.increment()
+                await click_coorditates(coordinates)
+                print(deerclicks)
+
+
+            if class_id == 6:
+                # Spooky clicked
+                spookyclicks.increment()
+                await click_coorditates(coordinates)
+                print(spookyclicks)
+
+            if class_id == 7:
+                # Bunnies clicked
+                bunnyclicks.increment()
+                await click_coorditates(coordinates)
+                print(bunnyclicks)
+
+            #     cv2.imwrite(str(output_folder.joinpath(format_datetime_for_filename()).with_suffix(".png")), frame)
+
+
+    # if len(boxes) > 5:
+    #     # Account for lots of cookies, click everything
+    #     return 0.01
+    return default_pause
             
 
-def makescreenshot():
+async def makescreenshot():
     """
     Makes a screenshot with MSS directly to the memory
     """
@@ -55,9 +103,24 @@ def makescreenshot():
         return png
 
 if __name__ == "__main__":
-    goldenclicks = Counter()
-    # time.sleep(3)
-    while True:
-        screenshot = makescreenshot()
-        predict(screenshot, goldenclicks)
-        time.sleep(10)
+    goldenclicks = Counter(1, "Golden cookies")
+    wrathclicks = Counter(2, "Wrath cookies")
+    handclicks = Counter(4, "Hands of Fate")
+    deerclicks = Counter(5, "Deers")
+    spookyclicks = Counter(6, "Spooky")
+    bunnyclicks = Counter(7, "Bunnies")
+
+    async def main():
+        while True:
+            screenshot = await makescreenshot()
+            pause = await predict(screenshot, 
+                                  goldenclicks, 
+                                  wrathclicks, 
+                                  handclicks, 
+                                  deerclicks, 
+                                  spookyclicks,
+                                  bunnyclicks)
+            
+            await asyncio.sleep(pause)
+
+    asyncio.run(main())
--- classes.py
+++ classes.py
@@ -1,17 +1,16 @@
 class Counter:
-    _instance = None
-    _counter = 0
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
-
+    def __init__(self, class_id, text):
+        self._counter = 0
+        self.text = text
+    
     @property
     def counter(self):
         return self._counter
-
+    
     def increment(self):
         self._counter += 1
+
+    def __str__(self):
+        return f"{self.text} clicked: {self._counter}"
 
 
